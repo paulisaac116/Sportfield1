6254e83075f23381faadb7a904a86cf467dbcdcf
paulisaac116 <paulgualab@gmail.com>
2021-10-14 22:50:53 -0500

    Login Page First Design Finished

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,103 +1,52 @@
+import React from 'react'
 import {useHistory}  from "react-router-dom";
-import { Button, Form, Image, Input } from "antd";
+import { GreenButton } from '../components/GreenButton'
 import  {auth} from '../firebase/index'
-import logo from "../images/sportfield_log.png";
 
-import "../styles/login.css";
-import "../styles/App.css";
 
+import '../styles/Login-RegisterPagesStyles/Login-RegisterPages.css'
+import sportfield_logo from '../images/sportfield_log.png'
 
 const LoginPage = () => {
 
-  let history = useHistory()
+    let history = useHistory()
+    const onFinish =  async (email, password) => {
 
-  const onFinish = async ({email, password}) => {
+        console.log(email)
+        console.log(password)
 
-    try {
-      const userCredential = await auth.signInWithEmailAndPassword(
-        email,
-        password
-      );
+        try {
+          const userCredential = await auth.signInWithEmailAndPassword(
+            email,
+            password
+          );
+    
+          // Signed in
+          const user = userCredential.user
+          console.log(user)
+          history.push("/profile")
+    
+        } catch (error) {
+          const errorCode = error.code;
+          console.log("errorCode", errorCode);
+        }
+      }
 
-      // Signed in
-      const user = userCredential.user
-      history.push("/profile")
 
-    } catch (error) {
-      const errorCode = error.code;
-      console.log("errorCode", errorCode);
-    }
-  }
-
-  return (
-    <div>
-       <div className="MainContainer">
-         <div className="imageContainer">
-           {" "}
-          <Image width={400} src={logo} left={800} top={300} />
-         </div>
-
-         <Form
-          name="basic"
-          labelCol={{
-            span: 8,
-          }}
-          wrapperCol={{
-            span: 16,
-          }}
-          initialValues={{
-            remember: true,
-          }}
-         onFinish={onFinish}
-          autoComplete="off"
-        >
-          <Form.Item
-            label="Correo"
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: "Por favor ingrese su correo electrónico!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: "Please input your password!",
-              },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-          <Form.Item
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          >
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-          <div className={"footer"}>¿No tienes cuenta? Regístrate </div>{" "}
-          <div
-            style={{ cursor: "pointer" }}
-            className={"footer1"}
-            //onClick={footer11}
-          >
-            {" "}
-            Aquí
-          </div>
-        </Form>
-      </div>
-    </div>
-  )
+    return (
+        <div className="LoginPage">
+            <div className="content">
+                <img className="content__logo" src={sportfield_logo} alt="sportfield logo"/>
+                <h1 id="title">Inicio de sesión</h1>
+                <form className="content__form">
+                    <input type="email" className="email" id="email" name="email" placeholder="Correo electrónico" />
+                    <input type="password" className="email" id="password" name="password" placeholder="Contraseña"/>
+                </form>
+                    <GreenButton button_name="Aceptar" button_func={() => onFinish(document.getElementById("email").value, document.getElementById("password").value) }/>
+                    <p>¿No tienes una cuenta? Regístrate <a href="/">AQUÍ</a></p>
+              </div>
+        </div>
+    )
 }
 
-export default LoginPage;
+export default LoginPage
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,148 +1,53 @@
 import React from 'react'
-import {useHistory}  from "react-router-dom";
-import { Form, Input, Row, Col, Image, message} from "antd";
-import  {auth, db} from '../firebase/index'
+import { GreenButton } from '../components/GreenButton'
 
-import { GreenButton } from "../components/GreenButton";
-import logo from "../images/sportfield_log.png";
+import '../styles/Login-RegisterPagesStyles/Login-RegisterPages.css'
+import sportfield_logo from '../images/sportfield_log.png'
 
-import "../styles/App.css";
-import "../styles/register.css";
+const RegisterPage = () => {
+  
+  const onFinish = () => {
 
-const { Item } = Form;
-
-function RegisterPage() {
- 
-  let history = useHistory()
-  const onFinish = async ({email, password, name, userName, land}) => {
-
-    try {
-      const userCredential = await auth.createUserWithEmailAndPassword(
-        email,
-        password
-      )
-      
-      // Signed in
-      const user = userCredential.user
-      const userId = user.uid
-
-      await db.collection("Users").doc(userId).set(
-        {
-          name,
-          email,
-          userName,
-          land
-        }
-      )
-      history.push("/profile")
-      message.success("Usuario registrado correctamente");
-
-    }catch (error) {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log("errorCode", errorCode);
-      console.log("errorMessage", errorMessage);
-    }
   }
 
   return (
-    <div>
-      <Image className="cimg" width={400} src={logo} left={300} />
-      <div className="Container1">
-        <Row>
-          <Col xs={1} sm={2} md={6} lg={7} />
-          <Col xs={22} sm={20} md={12} lg={10}>
-            {/* <h1>Registro de nuevo usuario</h1> */}
-            <Form
-              name={"form"}
-              initialValues={{
-                remember: true,
-              }}
-              onFinish={onFinish}
-              //onFinishFailed={on}
-            >
-              <Item
-                label="Usuario"
-                name="userName"
-                rules={[
-                  { required: true, message: "Ingresa un nombre de usuario válido (Ej. donnaza)" },
-                ]}
-              >
-                <Input />
-              </Item>
+    <div className="RegisterPage">
+      <div className="content">
+                <img className="content__logo" src={sportfield_logo} alt="sportfield logo"/>
+                <h1 id="title">Registro de nuevo usuario</h1>
+                <form className="content__form">
+                    <div className="content__form--row">
+                      <label htmlFor="userName">Usuario</label>
+                      <input type="text" className="userName" id="userName" name="userName"/>
+                    </div>
+                    <div className="content__form--row">
+                      <label htmlFor="name">Nombre</label>
+                      <input type="text" className="name" id="name" name="name"/>
+                    </div>
+                    <div className="content__form--row">
+                      <label htmlFor="lastName">Apellido</label>
+                      <input type="text" className="lastName" id="lastName" name="lastName"/>
+                    </div>
+                    <div className="content__form--row">
+                      <label htmlFor="email">Correo electrónico</label>
+                      <input type="email" className="email" id="email" name="email"/>
+                    </div>
 
-              <Item
-                label="Nombre"
-                name="name"
-                rules={[
-                  { required: true, message: "por favor ingresa tu Nombre" },
-                ]}
-              >
-                <Input />
-              </Item>
+                    <div className="content__form--row">
+                      <label htmlFor="password">Contraseña</label>
+                      <input type="password" className="email" id="password" name="password"/>
+                    </div>
 
-              <Item
-                label="Apellido"
-                name="lastname"
-                rules={[
-                  { required: true, message: "por favor ingresa tu Apellido" },
-                ]}
-              >
-                <Input />
-              </Item>
+                    <div className="content__form--row">
+                      <label htmlFor="land">Número de lote</label>
+                      <input type="number" className="land" id="land" name="land"/>
+                    </div>
 
-              <Item
-                label="Correo Electronico"
-                name="email"
-                rules={[
-                  {
-                    required: true,
-                    message: "por favor ingresa tu Correo Electronico",
-                  },
-                ]}
-              >
-                <Input />
-              </Item>
-
-              <Item
-                label="Contraseña"
-                name="password"
-                rules={[
-                  {
-                    required: true,
-                    message: "por favor ingresa tu Correo Contraseña",
-                  },
-                ]}
-              >
-                <Input.Password />
-              </Item>
-
-              <Item
-                label="Numero de lote"
-                name="land"
-                rules={[
-                  {
-                    required: true,
-                    message: "por favor ingresa tu Numero de lote",
-                  },
-                ]}
-              >
-                <Input />
-              </Item>
-
-              <Item>
-                <GreenButton
-                  button_name="Registrar"
-                  // button_func={}
-                />
-              </Item>
-            </Form>
-          </Col>
-
-          <Col xs={1} sm={2} md={6} lg={7} />
-        </Row>
+                </form>
+                <GreenButton button_name="Registrarse" button_func={() => onFinish()}/>
       </div>
     </div>
-  );
+  )
 }
-export default RegisterPage;
+
+export default RegisterPage
